c6e34908d2e2477855f2f21c73e9dc7be7e3eead
Gaalpos <gabripazos13@gmai.com>
2021-12-14 14:53:51 +0100

    al

---
+++ src/cap2/testCuenta.java
@@ -0,0 +1,25 @@
+package cap2;
+
+public class testCuenta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Cuenta dorada=new Cuenta();
+		Cuenta currela=new Cuenta("gabriel pazos", 2999);
+		
+		System.out.println(dorada.toString());
+		System.out.println(currela.toString());
+		System.out.println(Cuenta.getnCuentas());
+		
+		currela.deposito(150f);
+		System.out.println(currela.toString());
+		
+		currela.retiro(450f);
+		System.out.println(currela.toString());
+		
+		System.out.println(currela.saldo());
+	}
+
+}
