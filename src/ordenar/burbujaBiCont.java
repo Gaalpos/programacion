aa0fccab0f3767b3f7035062bdcd647456658f6d
Gaalpos <gabripazos13@gmai.com>
2022-01-26 14:54:40 +0100

    all

---
+++ src/ordenar/burbujaBiCont.java
@@ -0,0 +1,63 @@
+package ordenar;
+
+//Burbuja bidireccional
+public class burbujaBiCont {
+	
+	public static int []genera(int size) {
+		
+		int [] alea=new int[size];
+		for(int i=0;i<alea.length;i++) {
+			alea[i]=(int) (Math.random()*10);
+		}
+		return alea;
+	}
+	
+	public static void mostrar(int[] original) {
+		System.out.print("[ ");
+		for (int i = 0; i < original.length; i++)
+			System.out.print(original[i] + " ");
+		System.out.println("]");
+	}
+
+	public static int [] burbujaBi(int array[]) {
+		
+		int izq = 0;
+		int der = array.length - 1;
+		int aux;
+		int ultimo = array.length - 1;
+
+		do {
+			// bucle descentente
+			for (int i = izq; i < der; i++) {
+				if (array[i] > array[i + 1]) {
+					aux = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = aux;
+					ultimo = i;
+				}
+			}
+
+			// actualiza la posicion alcanzada en esa pasada del for
+			der = ultimo;
+			for (int j = der; j > izq; j--) {
+				if (array[j - 1] > array[j]) {
+					aux = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = aux;
+					ultimo = j;
+				}
+			}
+			// actualiza la posicion alcanzada en esa pasada del for
+			izq = ultimo;
+		} while (izq < der);
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] test=genera(100);
+		mostrar(test);
+		burbujaBi(test);
+		mostrar(test);
+	}
+}
---
+++ src/ordenar/burbujaFDescendente.java
@@ -0,0 +1,57 @@
+package ordenar;
+
+public class burbujaFDescendente {
+
+	public static void mostrar(float[] original) {
+		System.out.print("[ ");
+		for (int i = 0; i < original.length; i++)
+			System.out.print(original[i] + " ");
+		System.out.println("]");
+	}
+
+	public static float[] bubbleF(float[] array) {
+
+		// Bucle externo.
+
+		for (int i = 0; i < array.length - 1; i++) {
+
+			// Bucle interno.
+			// Se busca entre el resto de posiciones cuál es el valor más bajo.
+			for (int j = i + 1; j < array.length; j++) {
+			//	System.out.println("externo: " + i + " interno: " + j);
+				// SWAP
+				if (array[i] < array[j]) {
+					// Para intercambiar valores hay una variable auxiliar.
+					float cambio = array[i];
+					array[i] = array[j];
+					array[j] = cambio;
+				}
+				/*
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				*/
+			}
+		}
+		return array;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float[] notas = { 5.5f, 9f, 10f, 4.9f, 8f, 2f, 8.5f, 7f, 6.6f, 5f, 9f, 7f };
+
+		System.out.println("antes de empezar");
+		mostrar(notas);
+		
+		bubbleF(notas);
+		
+		System.out.println("después de ordenar");
+		mostrar(notas);
+
+	}
+
+}
