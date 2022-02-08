b7e68298a483c91345f3867a443687a56b6740aa
Gaalpos <gabripazos13@gmai.com>
2022-02-08 14:18:13 +0100

    fix

--- src/boletinArrays/ejercicio7.java
+++ src/boletinArrays/ejercicio7.java
@@ -9,9 +9,9 @@ public class ejercicio7 {
 
 	public static void main(String[] args) {
 
-		// int[] array = { 1, 3, 0, -5, 2, 0, -4, -7, 11, -2 };
+		int[] array = { 1, 3, 0, -5, 2, 0, -4, -7, 11, -2 };
 		//int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		int [] array = {10,9,8,7,6,5,4,3,2,1};
+		//int [] array = {10,9,8,7,6,5,4,3,2,1};
 
 		/*
 		 * i i+1 0 1 1 2 3 4
@@ -37,6 +37,6 @@ public class ejercicio7 {
 		else if (decrece)
 			System.out.println(" Es decreciente");
 		else
-			System.out.println("está desordenado");
+			System.out.println("Está desordenado");
 	}
 }
