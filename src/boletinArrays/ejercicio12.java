2a6b16e5b8af0125710709c11fc420de09f3d2ac
Gaalpos <gabripazos13@gmai.com>
2022-02-10 14:40:38 +0100

    todito

---
+++ src/boletinArrays/ejercicio12.java
@@ -0,0 +1,34 @@
+package boletinArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * Leer por teclado una tabla de 10 elementos numéricos
+enteros y una posición (entre 0 y 9). Eliminar el elemento
+situado en la posición dada sin dejar huecos.
+ */
+public class ejercicio12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int valor, indice=0;
+		
+		int[] array = {1,2,3,4,5,6,7,8,9,10 };
+		System.out.println(Arrays.toString(array));
+		
+
+		//Pedir indice
+		System.out.println("introduce un indice");
+		indice=sc.nextInt();
+		
+		
+		//quitar posicion
+		for(int i=0;i<indice;i++) {
+			
+		}
+	}
+
+}
