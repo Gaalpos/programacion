86dfba1e8ad7d43cb24e703499d7c272c57a15fc
Gaalpos <gabripazos13@gmail.com>
2022-03-03 17:05:56 +0100

    all

--- src/ejerciciosCadenas/quickSortChar.java
+++ src/ejerciciosCadenas/quickSortChar.java
@@ -1,6 +1,38 @@
 package ejerciciosCadenas;
 
 public class quickSortChar {
+	static void qsort(char[] items) {
+		qs(items, 0, items.length - 1);
+	}
 
-	qs(items, 0, items.lenght-1);
+	public static void qs(char items[], int left, int right) {
+
+		int i, j;
+		char x, y;
+
+		i = left;
+		j = right;
+		x = items[(left + right) / 2];
+
+		do {
+			while ((items[i] < x) && (i < right))
+				i++;
+			while ((x < items[j]) && (j > left))
+				j--;
+
+			if (i <= j) {
+				y = items[i];
+				items[i] = items[j];
+				items[j] = y;
+				i++;
+				j--;
+			}
+		} while (i <= j);
+
+		if (left < j)
+			qs(items, left, j);
+		if (i < right)
+			qs(items, i, right);
+	}
+	
 }
--- src/ejerciciosCadenas/quickSortDemo.java
+++ src/ejerciciosCadenas/quickSortDemo.java
@@ -1,39 +1,34 @@
 package ejerciciosCadenas;
 
+import java.util.Arrays;
+
 // CAE EN EXAMEN 100 por 100
 public class quickSortDemo {
 
-	static void qsort(char[] items) {
-		qs(items, 0, items.length - 1);
-	}
-
-	public static void qs(char items[], int left, int right) {
-
-		int i, j;
-		char x, y;
-
-		i = left;
-		j = right;
-		x = items[(left + right) / 2];
-
-		do {
-			while ((items[i] < x) && (i < right))
-				i++;
-			while ((x < items[j]) && (j > left))
-				j--;
-
-			if (i <= j) {
-				y = items[i];
-				items[i] = items[j];
-				items[j] = y;
-				i++;
-				j--;
-			}
-		} while (i <= j);
-
-		if (left < j)
-			qs(items, left, j);
-		if (i < right)
-			qs(items, i, right);
+	public static void main(String[] args) {
+		char a[]= {'d','x','a','r','p','j','i'};
+		int i;
+		
+		System.out.println(" Array original");
+		System.out.println(Arrays.toString(a));
+		
+		
+		for(i=0; i<a.length;i++) {
+			/*
+			System.out.println(a[i]);
+			System.out.println();
+			*/
+			quickSortChar.qsort(a);
+			
+		}
+		
+		
+		System.out.println(" Array ordenado");
+		System.out.println(Arrays.toString(a));
+		/*
+		for(i=0; i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		*/
 	}
 }
