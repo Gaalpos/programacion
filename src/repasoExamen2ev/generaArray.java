3048bc9060a4fa94a29c76676914d93769fcdaaf
Gaalpos <gabripazos13@gmail.com>
2022-03-20 12:08:25 +0100

    ALL

---
+++ src/repasoExamen2ev/generaArray.java
@@ -0,0 +1,15 @@
+package repasoExamen2ev;
+
+/*
+ * 2. Genera un array de 80 posiciones enteras
+ * que tomaran valores entre 0 y 300.
+ * Separe en dos matrices los valores pares e impares. 
+ * Ordene ambos arrays por el método Quicksort.
+ */
+public class generaArray {
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
