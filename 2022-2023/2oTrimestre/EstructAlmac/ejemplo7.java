c4c7d94d7ffaf95995cc14ad1ea5b72737e2c325
gaalpos <gabripazos13@gmail.com>
2023-01-10 14:53:41 +0100

    aññ

---
+++ 2022-2023/2oTrimestre/EstructAlmac/ejemplo7.java
@@ -0,0 +1,12 @@
+public class ejemplo7 {
+    public static void main(String[] args) {
+        String cad1 = "EMMA";
+        String cad2 = "EMMA";
+
+        if (cad1.equals(cad2)) {
+            System.out.println("son iguales");
+        } else {
+            System.out.println("no son iguales");
+        }
+    }
+}
