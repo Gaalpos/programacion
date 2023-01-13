f2acf56f659a9c465473cec9e6c3d248a92ab956
Anxo Campos <anxo.campos260204@gmail.com>
2023-01-13 14:05:19 +0100

    all

---
+++ 2022-2023/Anxito/ejercicios/siete/varios/varios1.java
@@ -0,0 +1,27 @@
+package varios;
+
+import java.util.Arrays;
+
+public class varios1 {
+
+	public static void main(String[] args) {
+		
+		int[] a = new int[20];
+		System.out.println(Arrays.toString(a));
+		
+		Arrays.fill(a, 23);
+		System.out.println(Arrays.toString(a));
+		
+		Arrays.fill(a, 3, 12, -5);
+		System.out.println(Arrays.toString(a));
+		
+		Arrays.sort(a, 2, 8);
+		System.out.println(Arrays.toString(a));
+		
+		Arrays.sort(a);
+		int pos = Arrays.binarySearch(a, 23);
+		System.out.println("indicce "+pos);
+		
+	}
+	
+}
