98bda289e9e62a5bd120736f16cb30dc49876f90
Anxo Campos <anxo.campos260204@gmail.com>
2023-01-16 14:07:54 +0100

    all

--- 2022-2023/Anxito/ejercicios/siete/varios/ejercicios61.java
+++ 2022-2023/Anxito/ejercicios/siete/varios/ejercicios61.java
@@ -34,6 +34,47 @@ public class ejercicios61 {
 			arrCh2[j]=(char)(97+j);
 		}
 		System.out.println(Arrays.toString(arrCh2));
+		
+		//opc 5
+		String cadena="";
+		for(int j=0; j<arrCh1.length; j++) {
+			cadena+= arrCh1[j];
+			System.out.println(cadena);
+		}
+		System.out.println(cadena);
+	
+	
+	//opc 6
+	
+	String cadena2="";
+	for(int j=0; j<arrCh1.length; j++) {
+		cadena2+= arrCh1[j];
+		System.out.println(cadena2);
+	}
+	System.out.println(cadena2);
+	
+	//opc 7
+	//intercalar los dos arrays
+	String cadena3 = "";
+	for(int j=0; j<arrCh1.length; j++) {
+		cadena3+=arrCh1[j];
+		cadena3+=arrCh2[j];
+		System.out.println(cadena3);
 	}
 	
+	//opc 8
+	//mostrar la suma de cada posicion o indice de arrDig1 o arrDig2
+	int[] dig3= new int[arrDig1.length];
+	
+	for(int k=0; k<arrDig1.length; k++) {
+		System.out.println(arrDig1[k]+arrDig2[k]);
+	}
+	
+	System.out.println(Arrays.toString (dig3));
+	
+	int[] suma =new int[arrDig1.length];
+	
+	//sumar=arrDig1+arrDig2;
+	
+	}
 }
