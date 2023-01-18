5f082656d1b41c9af9b0274e9225ed6b94f4f4d2
gaalpos <gabripazos13@gmail.com>
2023-01-18 14:47:37 +0100

    all

--- 2022-2023/2oTrimestre/ejercicios/ejercicio618.java
+++ 2022-2023/2oTrimestre/ejercicios/ejercicio618.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
 
-public class ejercicio612 {
+public class ejercicio618 {
+
+    public static int[] obtenerLongCadenas(String[] arr) {
+        int[] largos = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("largo de la cadena " + i + " es " + arr[i].length());
+            largos[i] += arr[i].length();
+        }
+
+        return largos;
+    }
 
     public static int obtenerSumaTotal(String[] arr) {
 
@@ -17,8 +29,11 @@ public class ejercicio612 {
 
         String parrafo = "sum has been the industry's standard dummy text ever since the 1500";
         String cads[] = parrafo.split(" ");
+        // String longos[] = parrafo.split(" ");
 
         System.out.println("total de caracteres:" + obtenerSumaTotal(cads));
+        System.out.println(Arrays.toString(obtenerLongCadenas(cads)));
 
     }
+
 }
