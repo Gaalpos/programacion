7b78a112efe2cf04bd5056f7a9c9d742e487dce0
Anxo Campos <anxo.campos260204@gmail.com>
2023-01-20 14:07:48 +0100

    all

--- 2022-2023/Anxito/ejercicios/siete/varios/ordenamientos.java
+++ 2022-2023/Anxito/ejercicios/siete/varios/ordenamientos.java
@@ -1,7 +1,54 @@
 package varios;
 
+import java.util.Arrays;
+
 public class ordenamientos {
 
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,5,6,7,5,3,5,6,7,8,3,3,5};
+		
+		System.out.println(Arrays.toString(arr));
+		
+		//burbuja(arr);
+		qSort(arr);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	//quickSort
+	public static void qSort(int items[]) {
+		qs(items,0 ,items.length-1);
+	}
+	
+	//version recursiva del metodo
+	private static void qs(int[] items, int left, int right ) {
+		
+		int i,j; //indices
+		int x,y; //valores
+		
+		i=left;
+		j=right;
+		x=items[(left+right)/2]; //posicion central
+		
+		do {
+			while((items[i]<x)  && (i<right)) i++;
+			while((x<items[j]) &&(j>left)) j--;
+			if(i<=j) {
+				y=items[i];
+				items[i] = items[j];
+				items[j]=y;
+				j++;
+				j--;
+			}
+		}while(i<=j);
+		
+		if(left<j) qs(items, left, j);
+		if(i<right) qs (items, i, right);
+		
+		}
+
+	
 	//bubble sort
 	
 	public static void burbuja(int[] arr) {
