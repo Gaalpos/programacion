ba00ae5a19d440fb7ddc965a89f4c40678c2db9e
Anxo Campos <anxo.campos260204@gmail.com>
2023-01-24 14:42:51 +0100

    all

--- 2022-2023/Anxito/ejercicios/siete/boletin/ejercicio4.java
+++ 2022-2023/Anxito/ejercicios/siete/boletin/ejercicio4.java
@@ -4,6 +4,23 @@ package boletin;
 siguiente orden: el primero, el último, el segundo, el 
 penúltimo, el tercero, etc. */
 
-public class ejercicio4 {
+import java.util.Scanner;
 
-}
+public class ejercicio4 {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int numero[] = new int[10];
+        for(int i=0;i<10;i++){
+        System.out.println("Introduzca numero");
+        numero[i] = sc.nextInt();
+        }
+        System.out.println("El resultado es");
+        for(int i=0;i<=4;i++){
+        System.out.println(numero[i]);
+        System.out.println(numero[9-i]);
+        }
+        }
+        }
+
---
+++ 2022-2023/Anxito/ejercicios/siete/boletin/ejercicio5.java
@@ -0,0 +1,13 @@
+package boletin;
+
+// 5. Leer por teclado dos tablas de 10 números enteros y
+// mezclarlas en una tercera de la forma: el 1o de A, el 1o de
+// B, el 2o de A, el 2o de B, etc.
+
+public class ejercicio5 {
+    
+public static void main(String[] args) {
+    
+}
+
+}
