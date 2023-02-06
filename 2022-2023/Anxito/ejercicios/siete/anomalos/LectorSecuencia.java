881e946819d1beef7fd6ea2ff204fd4a641990a8
Anxo Campos <anxo.campos260204@gmail.com>
2023-02-06 21:39:55 +0100

    all

--- 2022-2023/Anxito/ejercicios/siete/anomalos/LectorSecuencia.java
+++ 2022-2023/Anxito/ejercicios/siete/anomalos/LectorSecuencia.java
@@ -1,9 +1,17 @@
 package anomalos;
+import java.util.Scanner;
 
 public class LectorSecuencia {
-
-    public static void main(String[] args) {
-        
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese una frase: ");
+        String frase = sc.nextLine();
+        sc.close();
+    
+        String[] palabras = frase.split(" ");
+        for (String palabra : palabras) {
+          System.out.println(palabra);
+        }
+      }
     }
 
-}
--- 2022-2023/Anxito/ejercicios/siete/resueltos/palindromo.java
+++ 2022-2023/Anxito/ejercicios/siete/resueltos/palindromo.java
@@ -1,28 +1,30 @@
 package resueltos;
 
-// codigo para comprobar si una palabra es palindromo
+import java.util.Scanner;
 
 public class palindromo {
     
-    public static boolean esPalindrome(String dato){
-     StringBuffer sb = new StringBuffer(dato);
-        String reves = sb.reverse().toString();
-    
-        if(rev.equals(numero))   
-            return true;
-        else  
-            return false;
-    }
-    
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("dame un numero: ");
-    int numero = sc.nextInt();
-    
-    System.out.println("ES CAPICUA????? "+esCapicua(numero));
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Introduce una frase: ");
+        String s=sc.nextLine();
+        
+        System.out.print(s);
+        int fin = s.length()-1;
+        int inicio=0;
+        boolean espalin=true;
+        
+        while(inicio < fin){
+            if(s.charAt(inicio)!=s.charAt(fin)){
+                espalin=false;
+            }
+        inicio++;
+        fin--;
+        }
+        if(espalin)
+            System.out.print("\nEs palindromo.");
+        else
+            System.out.print("\nNo es palindromo.");
         
     }
 }
-
-
-
