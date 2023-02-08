508a351ef1ed3a14d641983b0ed16660902c749c
AlejandroBlancoV <alejandroblancovillar2004@gmail.com>
2023-02-08 01:23:14 +0100

    7

--- 2022-2023/Alejandro/Siete/ejemplos/Cadenas/ejercicio4A.java
+++ 2022-2023/Alejandro/Siete/ejemplos/Cadenas/ejercicio4A.java
@@ -1,5 +1,6 @@
 package Cadenas;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* Crea un programa en java que solicite al usuario dos cadenas de cacateres 
@@ -10,6 +11,17 @@ la parte que coincide con la segunda cadena introducida.*/
 
 public class ejercicio4A {
     
+    public static String compararCadenas(String[]arr,String[]arr2){
+        String uno="";
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==arr2[0]) arr[i]=arr[i].toUpperCase();
+            uno+=" "+arr[i];
+        }
+        
+        return uno;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -19,6 +31,11 @@ public class ejercicio4A {
         System.out.println("Dame una frase: ");
         String cad2 = sc.nextLine();
         
-        System.out.println(esPalindromo(palabra));
+        String[] arr=cad.split(" ");
+        String[] arr2=cad2.split(" ");
+
+        System.out.println(arr[0]);
+
+        System.out.println(compararCadenas(arr, arr2));
     }
 }
