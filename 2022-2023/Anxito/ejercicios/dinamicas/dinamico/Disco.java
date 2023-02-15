68064bc2d836b4338df154a159f21dc4b5d1a5f8
Anxo Campos <anxo.campos260204@gmail.com>
2023-02-15 14:42:59 +0100

    all

--- 2022-2023/Anxito/ejercicios/dinamicas/dinamico/Disco.java
+++ 2022-2023/Anxito/ejercicios/dinamicas/dinamico/Disco.java
@@ -6,69 +6,111 @@ package dinamico;
  * @author Luis José Sánchez
  */
 public class Disco {
-	
+
 	private String codigo = "LIBRE";
+	
 	private String autor;
+	
 	private String titulo;
+	
 	private String genero;
+	
 	private int duracion; // duración total en minutos
-
-	public String getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getAutor() {
-		return autor;
-	}
-
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-
-	public String getGenero() {
-		return genero;
-	}
-
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-
+	
+	public Disco() { }
+	
 	public Disco(String codigo, String autor, String titulo, String genero, int duracion) {
-        this.codigo = codigo;
-        this.autor = autor;
-        this.titulo = titulo;
-        this.genero = genero;
-        this.duracion = duracion;
-    }
-
-    public String getTitulo() {
-		return titulo;
+	
+	this.codigo = codigo;
+	
+	this.autor = autor;
+	
+	this.titulo = titulo;
+	
+	this.genero = genero;
+	
+	this.duracion = duracion;
+	
 	}
-
+	
+	public String getCodigo() {
+	
+	return codigo;
+	
+	}
+	
+	public void setCodigo(String codigo) {
+	
+	this.codigo = codigo;
+	
+	}
+	
+	public String getAutor() {
+	
+	return autor;
+	
+	}
+	
+	public void setAutor(String autor) {
+	
+	this.autor = autor;
+	
+	}
+	
+	public String getGenero() {
+	
+	return genero;
+	
+	}
+	
+	public void setGenero(String genero) {
+	
+	this.genero = genero;
+	
+	}
+	
+	public String getTitulo() {
+	
+	return titulo;
+	
+	}
+	
 	public void setTitulo(String titulo) {
-		this.titulo = titulo;
+	
+	this.titulo = titulo;
+	
 	}
-
+	
 	public int getDuracion() {
-		return duracion;
+	
+	return duracion;
+	
 	}
-
+	
 	public void setDuracion(int duracion) {
-		this.duracion = duracion;
+	
+	this.duracion = duracion;
+	
 	}
-
+	
 	public String toString() {
-		String cadena = "\n------------------------------------------";
-		cadena += "\nCódigo: " + this.codigo;
-		cadena += "\nAutor: " + this.autor;
-		cadena += "\nTítulo: " + this.titulo;
-		cadena += "\nGénero: " + this.genero;
-		cadena += "\nDuración: " + this.duracion;
-		cadena += "\n------------------------------------------";
-		return cadena;
+	
+	String cadena = "\n------------------------------------------";
+	
+	cadena += "\nCódigo: " + this.codigo;
+	
+	cadena += "\nAutor: " + this.autor;
+	
+	cadena += "\nTítulo: " + this.titulo;
+	
+	cadena += "\nGénero: " + this.genero;
+	
+	cadena += "\nDuración: " + this.duracion;
+	
+	cadena += "\n------------------------------------------";
+	
+	return cadena;
+	
 	}
-}
+	
+	}
--- 2022-2023/Anxito/ejercicios/dinamicas/dinamico/coleccionDiscos.java
+++ 2022-2023/Anxito/ejercicios/dinamicas/dinamico/coleccionDiscos.java
@@ -3,19 +3,35 @@ package dinamico;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Realiza de nuevo el ejercicio de la colección de discos 
-// pero utilizando esta vez 
-// una lista para almacenar la información sobre los 
-// discos en lugar de un array 
-// convencional. Comprobarás que el código se simplifica 
-// notablemente ¿Cuánto 
-// ocupa el programa original hecho con un array? ¿Cuánto 
-// ocupa este nuevo 
-// programa hecho con una lista? 
-
 public class coleccionDiscos {
 
-    public static void main(String[] args) {
+	/*
+	 * Ejercicio 5 Realiza de nuevo el ejercicio de la colección de discos pero
+	 * utilizando esta vez una lista para almacenar la información sobre los discos
+	 * en lugar de un array convencional. Comprobarás que el código se simplifica
+	 * notablemente ¿Cuánto ocupa el programa original hecho con un array? ¿Cuánto
+	 * ocupa este nuevo programa hecho con una lista?
+	 */
+
+	public static String comprobarCodigo(Disco[] discos) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("Por favor, introduzca el código del disco que desea borrar/modificar: ");
+        String code = s.nextLine();
+  
+        int i = -1;
+        do {
+          i++;
+        } while (!((discos[i].getCodigo()).equals(code)) && i<discos.length );
+
+		return code;
+
+		return "Codigo no encontrado";
+	}
+
+
+
+	public static void main(String[] args) {
 
 		// N determina el tamaño del array
 		final int N = 100;
@@ -28,10 +44,8 @@ public class coleccionDiscos {
 			discos[i] = new Disco();
 		}
 
-        //opcion de menu
 		int opcion;
 		Scanner s = new Scanner(System.in);
-        //variables para un objeto de la clase disco 
 		String codigoIntroducido;
 		String autorIntroducido;
 		String tituloIntroducido;
@@ -48,15 +62,120 @@ public class coleccionDiscos {
 			System.out.println("5. Salir");
 			System.out.print("Introduzca una opción: ");
 			opcion = Integer.parseInt(s.nextLine());
-
+		
+		
 			switch (opcion) {
+			
 			case 1:
 				System.out.println("\nLISTADO");
 				System.out.println("=======");
+				   for(Disco d : discos) {
+					if(!d.getCodigo().equals("LIBRE")) {
+						System.out.println(d.toString());
+					}
+				   
+              }              
+              break;
+			
+	
+	case 2:
+        System.out.println("\nNUEVO DISCO");
+        System.out.println("===========");
+        
+        // Busca la primera posición libre del array
+        int primeraLibre = -1;
+        do {
+          primeraLibre++;
+		  System.out.println("indice del array: "+primeraLibre);
+        } while (!((discos[primeraLibre].getCodigo()).equals("LIBRE")));
+        
+        System.out.println("Por favor, introduzca los datos del disco.");  
+        System.out.print("Código: ");
+        codigoIntroducido = s.nextLine();
+        System.out.print("Autor: ");
+        autorIntroducido = s.nextLine();
+        System.out.print("Título: ");
+        tituloIntroducido = s.nextLine();
+        System.out.print("Género: ");
+        generoIntroducido = s.nextLine();
+        System.out.print("Duración: ");
+        duracionIntroducida = Integer.parseInt(s.nextLine());
+        discos[primeraLibre] = new Disco(
+          codigoIntroducido, autorIntroducido, tituloIntroducido, generoIntroducido, duracionIntroducida);
+        
+        break;
 
-			default:
+	case 3:
+		System.out.println("\nMODIFICAR");
+        System.out.println("===========");
+        
+        System.out.print("Por favor, introduzca el código del disco cuyos datos desea cambiar: ");
+        codigoIntroducido = s.nextLine();
+  
+        int i = -1;
+        do {
+          i++;
+        } while (!((discos[i].getCodigo()).equals(codigoIntroducido)));
+        
+        System.out.println("Introduzca los nuevos datos del disco o INTRO para dejarlos igual.");
+  
+        System.out.println("Código: " + discos[i].getCodigo());
+        System.out.print("Nuevo código: ");
+        codigoIntroducido = s.nextLine();
+        if (!codigoIntroducido.equals("")) {
+          discos[i].setCodigo(codigoIntroducido);
+        }
+        
+        System.out.println("Autor: " + discos[i].getAutor());
+        System.out.print("Nuevo autor: ");
+        autorIntroducido = s.nextLine();
+        if (!autorIntroducido.equals("")) {
+          discos[i].setAutor(autorIntroducido);
+        }
+        
+        System.out.println("Título: " + discos[i].getTitulo());
+        System.out.print("Nuevo título: ");
+        tituloIntroducido = s.nextLine();
+        if (!tituloIntroducido.equals("")) {
+          discos[i].setTitulo(tituloIntroducido);
+        }
+        
+        System.out.println("Género: " + discos[i].getGenero());
+        System.out.print("Nuevo género: ");
+        generoIntroducido = s.nextLine();
+        if (!generoIntroducido.equals("")) {
+          discos[i].setGenero(generoIntroducido);
+        }
+        
+        System.out.println("Duración: " + discos[i].getDuracion());
+        System.out.print("Duración: ");
+        final String duracionIntroducidaString = s.nextLine();
+        if (!duracionIntroducidaString.equals("")) {
+          discos[i].setDuracion(Integer.parseInt(duracionIntroducidaString));
+        }
+        
+        break;
 
-			} // switch
-		} while (opcion != 5);
+		case 4:
+		System.out.println("\nBORRAR");
+        System.out.println("======");
+        
+        System.out.print("Por favor, introduzca el código del disco que desea borrar: ");
+        codigoIntroducido = s.nextLine();
+  
+        i = -1;
+        do {
+          i++;
+        } while (!((discos[i].getCodigo()).equals(codigoIntroducido))	);
+        discos[i].setCodigo("LIBRE");
+        System.out.println("Album borrado.");
+
+       	 	break;
+		default:
+
+			}//switch
+		}while(opcion!=5);
+
 	}
 }
+
