d0120c84fc0daad0cae6008bb83956d05a52ed12
Laura Cascallana <lauracascallanaf@gmail.com>
2023-03-13 13:04:44 +0100

    13

---
+++ 2022-2023/Alejandro/Colecciones/Listas/Listas/Mapas/ejemplo1.java
@@ -0,0 +1,23 @@
+package Colecciones.Listas.Listas.Mapas;
+
+import java.util.HashMap;
+
+public class ejemplo1 {
+    public static void main(String[] args) {
+		
+		//declaramos un mapa
+		HashMap<Integer, String> m = new HashMap<Integer, String>();
+		
+		//metodo para añadir parejas al map
+		m.put(924, "Amalia Núñez");
+		m.put(921, "Cindy Nero");
+		m.put(700, "César Vázquez");
+		m.put(219, "Víctor Tilla");
+		m.put(537, "Alan Brito");
+		m.put(605, "Esteban Quito ");
+		
+		System.out.println("Los elementos de m son: \n" + m);
+	}
+
+    // ME GUSTAS BEBE, BOOOF Q BAKUGANS
+}
