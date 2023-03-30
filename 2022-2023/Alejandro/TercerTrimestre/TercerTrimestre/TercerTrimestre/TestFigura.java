14df7d99226b7907e9d081d2a4348759f1679a65
Laura Cascallana <lauracascallanaf@gmail.com>
2023-03-30 14:06:00 +0200

    30

---
+++ 2022-2023/Alejandro/TercerTrimestre/TercerTrimestre/TercerTrimestre/TestFigura.java
@@ -0,0 +1,16 @@
+package TercerTrimestre;
+
+import java.awt.Color;
+
+public class TestFigura {
+    public static void main(String[] args) {
+        Figura uno = new Figura(Color.GREEN);
+        int[] centro={1,2};
+
+        Figura dos = new Figura(Color.BLUE, centro );
+        System.out.println(uno);
+        System.out.println(dos);
+
+
+    }
+}
