9b761c0eb9042f3f4e4a1a255863ef439d3eb397
gaalpos <gabripazos13@gmail.com>
2023-04-11 13:43:00 +0200

    04/11/2023 13:43:00

---
+++ 2022-2023/3erTrimestre/Logistica.java
@@ -0,0 +1,51 @@
+public class Logistica {
+
+  private String Nombre;
+  private byte Capacidad;
+  private byte NumeroDeHuecos;
+
+  Logistica(byte Capacidad, String n) {
+    this.Capacidad = Capacidad;
+    NumeroDeHuecos = Capacidad;
+    this.Nombre = n;
+  }
+
+  public byte getNumeroDeHuecos() {
+    return (NumeroDeHuecos);
+  }
+
+  public byte getCapacidad() {
+    return (Capacidad);
+  }
+
+  public boolean HayHueco() {
+    return (NumeroDeHuecos != 0);
+  }
+
+  public boolean HayContenedor() {
+    return (NumeroDeHuecos != Capacidad);
+  }
+
+  public void MeteContenedor() {
+    System.out.println(this.toString());
+    NumeroDeHuecos--;
+  }
+
+  public void SacaContenedor() {
+    System.out.println(this.toString());
+    NumeroDeHuecos++;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "Logistica [Nombre=" +
+      Nombre +
+      ", Capacidad=" +
+      Capacidad +
+      ", NumeroDeHuecos=" +
+      NumeroDeHuecos +
+      "]"
+    );
+  }
+} // LogisticaAlmacen
