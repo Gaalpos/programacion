bec0248001f443231af77b5f87afbb50609a7701
Alejandro Blanco <alejandroblancovillar2004@gmail.com>
2023-04-19 10:38:55 +0200

    19

--- 2022-2023/Alejandro/TercerTrimestre/TercerTrimestre/TercerTrimestre/Capitulo5/Ejemplo3/Camion.java
+++ 2022-2023/Alejandro/TercerTrimestre/TercerTrimestre/TercerTrimestre/Capitulo5/Ejemplo3/Camion.java
@@ -1,44 +1,49 @@
-package Capitulo5.Ejemplo3;
 
 import java.awt.Color;
 
 public class Camion extends Vehiculo {
 
-private byte NumeroDeEjes;
+    private byte NumeroDeEjes;
 
-Camion(byte NumeroDeRuedas) {
-super(NumeroDeRuedas);
-}
+    Camion(byte NumeroDeRuedas) {
+        super(NumeroDeRuedas);
+    }
 
-Camion(Color color, byte NumeroDeRuedas) {
-super(color,NumeroDeRuedas);
-}
-Camion(Color color, byte NumeroDeRuedas,
-short Cilindrada) {
-super(color,NumeroDeRuedas,Cilindrada);
-}
+    Camion(Color color, byte NumeroDeRuedas) {
+        super(color, NumeroDeRuedas);
+    }
 
-Camion(Color color, byte NumeroDeRuedas,
-short Cilindrada, short Potencia) {
-super(color,NumeroDeRuedas,Cilindrada,Potencia);
+    Camion(Color color, byte NumeroDeRuedas,
+            short Cilindrada) {
+        super(color, NumeroDeRuedas, Cilindrada);
+    }
 
-}
+    Camion(Color color, byte NumeroDeRuedas,
+            short Cilindrada, short Potencia) {
+        super(color, NumeroDeRuedas, Cilindrada, Potencia);
 
-Camion(Color color, byte NumeroDeRuedas,
-byte NumeroDeEjes, short Cilindrada,
-short Potencia) {
-super(color,NumeroDeRuedas,Cilindrada,Potencia);
-EstableceNumeroDeEjes(NumeroDeEjes);
-}
+    }
 
-public byte DimeNumeroDeEjes() {
-return NumeroDeEjes;
-}
+    Camion(Color color, byte NumeroDeRuedas,
+            byte NumeroDeEjes, short Cilindrada,
+            short Potencia) {
+        super(color, NumeroDeRuedas, Cilindrada, Potencia);
+        EstableceNumeroDeEjes(NumeroDeEjes);
+    }
 
-public void EstableceNumeroDeEjes(byte NumeroDeEjes) {
-this.NumeroDeEjes = NumeroDeEjes;
-}
+    public byte DimeNumeroDeEjes() {
+        return NumeroDeEjes;
+    }
 
+    public void EstableceNumeroDeEjes(byte NumeroDeEjes) {
+        this.NumeroDeEjes = NumeroDeEjes;
+    }
 
+    @Override
+    public String toString() {
+        return "Camion [NumeroDeEjes=" + NumeroDeEjes + "ColorVehiculo=" + ColorVehiculo + ", NumRuedas=" + NumRuedas
+                + ", Cilindrada=" + Cilindrada
+                + ", Potencia=" + Potencia + "]";
+    }
 
 }
--- 2022-2023/Alejandro/TercerTrimestre/TercerTrimestre/TercerTrimestre/Capitulo5/Ejemplo3/Vehiculo.java
+++ 2022-2023/Alejandro/TercerTrimestre/TercerTrimestre/TercerTrimestre/Capitulo5/Ejemplo3/Vehiculo.java
@@ -1,64 +1,69 @@
-package Capitulo5.Ejemplo3;
+
 import java.awt.Color;
+
 public class Vehiculo {
-    private Color ColorVehiculo;
- private byte NumRuedas;
- private short Cilindrada;
- private short Potencia;
+    protected Color ColorVehiculo;
+    protected byte NumRuedas;
+    protected short Cilindrada;
+    protected short Potencia;
 
-Vehiculo(Color color) {
-    setColor(color);
-}
+    Vehiculo(Color color) {
+        setColor(color);
+    }
 
-Vehiculo (byte NumRuedas) {
-this.NumRuedas = NumRuedas;
-}
+    Vehiculo(byte NumRuedas) {
+        this.NumRuedas = NumRuedas;
+    }
 
-Vehiculo (short Cilindrada) {
-this.Cilindrada = Cilindrada;
-}
+    Vehiculo(short Cilindrada) {
+        this.Cilindrada = Cilindrada;
+    }
 
-Vehiculo(Color color, byte NumRuedas) {
-this(color);
-this.NumRuedas = NumRuedas;
-}
+    Vehiculo(Color color, byte NumRuedas) {
+        this(color);
+        this.NumRuedas = NumRuedas;
+    }
 
-Vehiculo(Color color, byte NumRuedas, short Cilindrada) {
-this(color,NumRuedas);
-this.Cilindrada = Cilindrada;
-}
+    Vehiculo(Color color, byte NumRuedas, short Cilindrada) {
+        this(color, NumRuedas);
+        this.Cilindrada = Cilindrada;
+    }
 
-Vehiculo(Color color, byte NumRuedas, short Cilindrada,
-short Potencia) {
-this(color,NumRuedas,Cilindrada);
-this.Potencia = Potencia;
+    Vehiculo(Color color, byte NumRuedas, short Cilindrada,
+            short Potencia) {
+        this(color, NumRuedas, Cilindrada);
+        this.Potencia = Potencia;
 
-}
+    }
 
-public void setColor(Color color) {
-ColorVehiculo = color;
-}
-public Color getColor() {
-return ColorVehiculo;
-}
+    public void setColor(Color color) {
+        ColorVehiculo = color;
+    }
 
-public byte getNumRuedas() {
-return NumRuedas;
-}
+    public Color getColor() {
+        return ColorVehiculo;
+    }
 
-public short getCilindrada() {
-return Cilindrada;
-}
+    public byte getNumRuedas() {
+        return NumRuedas;
+    }
 
-public short getPotencia() {
-return Potencia;
-}
+    public short getCilindrada() {
+        return Cilindrada;
+    }
 
-@Override
-public String toString() {
-    return "Vehiculo [ColorVehiculo=" + ColorVehiculo + ", NumRuedas=" + NumRuedas + ", Cilindrada=" + Cilindrada
-            + ", Potencia=" + Potencia + "]";
-}
+    public short getPotencia() {
+        return Potencia;
+    }
 
+    public double pagarRodaje() {
+        return 53.27d;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo [ColorVehiculo=" + ColorVehiculo + ", NumRuedas=" + NumRuedas + ", Cilindrada=" + Cilindrada
+                + ", Potencia=" + Potencia + "]";
+    }
 
 }
